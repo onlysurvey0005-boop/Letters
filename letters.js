bf639490ff18fcdb1b1ff2e4933989dc76f6cffb
onlysurvey0005-boop <onlysurvey0005@gmail.com>
2025-12-07 18:27:28 +0530

    Update letters.js

--- letters.js
+++ letters.js
@@ -1,1 +1,42 @@
+export const letters = [
+            {
+                day: 1,
+                title: "Day 1: The first thing I love about you",
+                content: `Attitude.`,
+                color: "gradient-1"
+            },
+            {
+                day: 2,
+                title: "Day 2: Your name given by me",
+                content: `Micky.`,
+                color: "gradient-2"
+            },
+            {
+                day: 3,
+                title: "Day 3: an animal name",
+                content: `Mankidi.`,
+                color: "gradient-3"
+            },
+            {
+                day: 4,
+                title: "Day 4:MOst of the time you tell me a word",
+                content: `Alazya.`,
+                color: "gradient-4"
+            },
+            {
+                day: 5,
+                title: "Day 5: our first meet on which festival",
+                content: `Rakshya bandhan.`,
+                color: "gradient-5"
+            }
+        ];
 
+
+
+export const wordPuzzles = {
+            1: { word: "GUESS", hint: "Hint that only the two of us know!" },
+            2: { word: "GUESS", hint: "Hint that only the two of us know!" },
+            3: { word: "GUESS", hint: "Hint that only the two of us know!" },
+            4: { word: "GUESS", hint: "Hint that only the two of us know!" },
+            5: { word: "GUESS", hint: "Hint that only the two of us know!" }
+        };
